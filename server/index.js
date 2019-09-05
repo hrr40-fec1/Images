8bb202ed54aa2f524f21af57dfc6feaa841f46af
Logan Dudley <logandudley@gmail.com>
2019-09-04 17:43:32 -0700

    commit server/index.js

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+const port = 3003;
+const db = require('../database/index.js');
+
+let allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+ }
+ app.use(allowCrossDomain);
+
+app.get('/api/images/:productId', (req, res) => {
+  const { productId } = req.params;
+  console.log('proid: ', productId);
+
+  db.findOne({ productId: productId }, (err, results) => {
+    if (err || results.length === 0) {
+      console.log('Error finding shirt', err);
+      res.sendStatus(404);
+    } else {
+      console.log('results: ', results);
+      res.send(results.imageURLs);
+    }
+  });
+});
+
+app.listen(port, () => console.log(`Images Server listening on ${port}`));
